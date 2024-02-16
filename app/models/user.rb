d9dc0b72c5c35e2bab9e911c931328522c127971
masukazuno <wnsxa43351@gmail.com>
2024-02-16 22:37:09 +0900

    コミット

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthday, presence: true
+  validates :last_name, :first_name, :ruby_last_name, :ruby_first_name, presence: { message: 'は必須項目です。' }
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力して下さい。' }
+  validates :ruby_last_name, :ruby_first_name, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+       end
+       
---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+    factory :user do
+      # nickname {Faker::Name.nickname}
+      nickname {"ABC"}
+      email {Faker::Internet.email}
+      password {Faker::Internet.password(min_length: 6)}
+      password_confirmation {password}
+      last_name{"山田"}
+      first_name{"太郎"}
+      ruby_last_name{"ヤマダ"}
+      ruby_first_name{"タロウ"}
+      birthday{"19890723"}
+    end
+  end 
+  
