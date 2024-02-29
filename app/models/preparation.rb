98d103e8b9d990e0e19389b75a06b8022693d42c
masukazuno <wnsxa43351@gmail.com>
2024-02-29 20:49:40 +0900

    コミット

---
+++ app/models/preparation.rb
@@ -0,0 +1,14 @@
+class Preparation < ActiveHash::Base
+
+  include ActiveHash::Associations
+   has_many :items
+
+   self.data = [
+    { id: 1, preparation_day: '---' },
+    { id: 2, preparation_day: '1~2日で発送' },
+    { id: 3, preparation_day: '2~3日で発送' },
+    { id: 4, preparation_day: '4~7日で発送' }
+  ]
+  include ActiveHash::Associations
+  has_many :items
+end
