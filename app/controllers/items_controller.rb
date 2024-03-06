1aacb062ea52ae929c7409a01dc78300e7af63ba
masukazuno <wnsxa43351@gmail.com>
2024-03-06 16:40:14 +0900

    コミット
    コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show,:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :set_item, only: [:show,:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -28,6 +28,11 @@ class ItemsController < ApplicationController
         render :edit, status: :unprocessable_entity
       end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to action: :index
+  end
   
     def create
       @item = Item.new(item_params)
@@ -38,8 +43,6 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-    end
-
 
   private
   
@@ -50,3 +53,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+end
