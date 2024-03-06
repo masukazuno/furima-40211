a8804605f1082c9e304d9610cb242ce4b45305db
masukazuno <wnsxa43351@gmail.com>
2024-03-06 09:39:33 +0900

    コミット
    コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show,:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -15,6 +15,20 @@ class ItemsController < ApplicationController
     
     end
 
+    def edit
+      unless current_user.id == @item.user_id
+        redirect_to action: :index
+      end
+    end
+  
+    def update
+      if @item.update(item_params)
+        redirect_to item_path(params[:id])
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+  
     def create
       @item = Item.new(item_params)
     if @item.save
@@ -28,6 +42,7 @@ class ItemsController < ApplicationController
 
 
   private
+  
   def item_params
     params.require(:item).permit(:name, :introduction, :image, :price, :item_condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :category_id).merge(user_id: current_user.id)
   end
